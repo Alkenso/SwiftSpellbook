cdee07bf464fe40dc15b4797078e03d831a202ec
Alkenso (Vladimir Vashurkin) <alkensox@gmail.com>
2021-06-04 11:39:13 +0300

    Covered Resource(RAII) with docs.

--- Sources/SwiftConvenience/Resource (RAII).swift
+++ Sources/SwiftConvenience/Resource (RAII).swift
@@ -11,18 +11,31 @@ public class Resource<T> {
         _cleanup = cleanup
     }
     
+    /// Immediately perform cleanup action.
+    func forceCleanup() {
+        let cleanup = __cleanup.exchange { _ in }
+        cleanup(value)
+    }
+    
+    /// Disables cleanup action when Resource is freed.
+    func cancelCleanup() {
+        _cleanup = { _ in }
+    }
+    
     deinit {
         _cleanup(value)
     }
     
-    private let _cleanup: (T) -> Void
+    @Atomic private var _cleanup: (T) -> Void
 }
 
 public extension Resource {
+    /// Create Resource wrapping value and performing cleanup block when the wrapper if freed.
     static func raii(_ value: T, cleanup: @escaping (T) -> Void) -> Resource {
         Resource(value, cleanup: cleanup)
     }
     
+    /// Create Resource wrapping value and performing nothing when the wrapper if freed.
     static func stub(_ value: T) -> Resource {
         Resource(value, cleanup: { _ in })
     }
@@ -35,20 +48,24 @@ public extension Resource where T == Void {
         self.init((), cleanup: onDeinit)
     }
     
+    /// Perform the action when the instance is freed.
     static func onDeinit(_ action: @escaping () -> Void) -> DeinitAction {
         Resource(action)
     }
 }
 
 public extension Resource where T == URL {
+    /// Create Resource wrapping URL and removing it when the wrapper if freed.
     static func raii(location filesystemURL: URL) -> Resource {
         raii(filesystemURL) { try? FileManager.default.removeItem(at: $0) }
     }
     
-    static func raii(temporaryFile: URL) throws -> Resource {
-        let owned = TemporaryDirectory().uniqueFile(name: temporaryFile.lastPathComponent)
+    /// Create Resource wrapping URL and removing it when the wrapper if freed.
+    /// The item will be moved into temporary location first.
+    static func raii(moving url: URL) throws -> Resource {
+        let owned = TemporaryDirectory().uniqueFile(name: url.lastPathComponent)
         try owned.createDirectoryTree()
-        try FileManager.default.moveItem(at: temporaryFile, to: owned.url)
+        try FileManager.default.moveItem(at: url, to: owned.url)
         return raii(location: owned.url)
     }
 }
