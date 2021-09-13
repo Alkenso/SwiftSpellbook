91fdd4aca02409bbd4c115911930cc1767b76dd7
Alkenso (Vladimir Vashurkin) <alkensox@gmail.com>
2021-09-13 20:22:35 +0300

    Extended audit_token_t

--- Package.swift
+++ Package.swift
@@ -10,26 +10,37 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SwiftConvenience",
-            targets: ["SwiftConvenience"]),
+            targets: ["SwiftConvenience"]
+        ),
         .library(
             name: "SwiftConvenienceTestUtils",
-            targets: ["SwiftConvenienceTestUtils"]),
+            targets: ["SwiftConvenienceTestUtils"]
+        ),
     ],
     dependencies: [
     ],
     targets: [
         .target(
             name: "SwiftConvenience",
-            dependencies: []),
+            dependencies: []
+        ),
+        .target(
+            name: "SwiftConvenienceObjcBridge",
+            dependencies: []
+        ),
         .target(
             name: "SwiftConvenienceTestUtils",
-            dependencies: []),
+            dependencies: []
+        ),
         .testTarget(
             name: "SwiftConvenienceTests",
             dependencies: ["SwiftConvenience", "SwiftConvenienceTestUtils"],
-            linkerSettings: [LinkerSetting.linkedLibrary("bsm", .when(platforms: [.macOS]))]),
+            linkerSettings: [LinkerSetting.linkedLibrary("bsm", .when(platforms: [.macOS]))]
+        ),
         .testTarget(
             name: "SwiftConvenienceTestUtilsTests",
-            dependencies: ["SwiftConvenience", "SwiftConvenienceTestUtils"]),
+            dependencies: ["SwiftConvenience", "SwiftConvenienceTestUtils"],
+            linkerSettings: [LinkerSetting.linkedLibrary("bsm", .when(platforms: [.macOS]))]
+        ),
     ]
 )
--- Sources/SwiftConvenience/Low Level/Mach.swift
+++ Sources/SwiftConvenience/Low Level/Mach.swift
@@ -46,6 +46,17 @@ public extension audit_token_t {
     }
 }
 
+public extension audit_token_t {
+    var auid: uid_t { audit_token_to_auid(self) }
+    var euid: uid_t { audit_token_to_euid(self) }
+    var egid: gid_t { audit_token_to_egid(self) }
+    var ruid: uid_t { audit_token_to_ruid(self) }
+    var rgid: gid_t { audit_token_to_rgid(self) }
+    var pid: pid_t { audit_token_to_pid(self) }
+    var asid: au_asid_t { audit_token_to_asid(self) }
+    var pidversion: Int32 { audit_token_to_pidversion(self) }
+}
+
 extension audit_token_t: Hashable {
     public static func == (lhs: audit_token_t, rhs: audit_token_t) -> Bool {
         withUnsafeBytes(of: lhs) { first -> Bool in
