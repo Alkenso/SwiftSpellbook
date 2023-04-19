7e5dc5f9ee128c07fb712a286a43073ffe82e531
Alkenso (Vladimir Vashurkin) <alkensox@gmail.com>
2023-04-19 18:42:04 +0300

    Extend `EventAsk` with specifyng timeout and fallback on asks

--- Sources/SwiftConvenience/ValueObserving/EventAsk.swift
+++ Sources/SwiftConvenience/ValueObserving/EventAsk.swift
@@ -27,11 +27,15 @@ public typealias EventAskCombined<Input, Output> = EventAskEx<Input, Output, Out
 public typealias EventAsk<Input, Output> = EventAskEx<Input, Output, [Output]>
 
 public class EventAskEx<Input, Transformed, Output> {
+    private typealias Entry<T> = (transform: AsyncTransform, queue: DispatchQueue)
+    private let transforms = Synchronized<[ObjectIdentifier: Entry<AsyncTransform>]>(.concurrent)
+    private let combine: ([Transformed]) -> Output
+    
     public typealias AsyncTransform = (Input, @escaping (Transformed) -> Void) -> Void
     public typealias SyncTransform = (Input) -> Transformed
     
     public init(combine: @escaping ([Transformed]) -> Output) {
-        _combine = combine
+        self.combine = combine
     }
     
     public convenience init() where Output == [Transformed] {
@@ -40,42 +44,61 @@ public class EventAskEx<Input, Transformed, Output> {
     
     // MARK: Ask
     
-    public func askAsync(_ value: Input, receive queue: DispatchQueue = .global(), completion: @escaping (Output) -> Void) {
-        transform(value, queue: queue, completion: completion)
+    public func askAsync(_ value: Input, receive queue: DispatchQueue = .global(), timeout: Timeout? = nil, completion: @escaping (Output) -> Void) {
+        transform(value, queue: queue, timeout: timeout, completion: completion)
     }
     
-    public func askSync(_ value: Input) -> Output {
+    public func askSync(_ value: Input, timeout: Timeout? = nil) -> Output {
         var output: Output!
-        transform(value, queue: nil) { output = $0 }
+        transform(value, queue: nil, timeout: timeout) { output = $0 }
         return output
     }
     
-    private func transform(_ value: Input, queue: DispatchQueue?, completion: @escaping (Output) -> Void) {
-        let transforms = _transforms.read { $0.values }
-        var transformedValues: [Transformed?] = .init(repeating: nil, count: transforms.count)
+    private func transform(_ value: Input, queue: DispatchQueue?, timeout: Timeout?, completion: @escaping (Output) -> Void) {
+        let transforms = transforms.read { $0.values }
         
+        let values = Values(count: transforms.count)
         let group = DispatchGroup()
         for (idx, entry) in transforms.enumerated() {
             group.enter()
             entry.queue.async {
                 entry.transform(value) { singleResult in
-                    transformedValues[idx] = singleResult
+                    values.set(value: singleResult, at: idx)
                     group.leave()
                 }
             }
         }
         
-        let submitResult = { [_combine] in
-            let combined = _combine(transformedValues.compactMap { $0 })
-            completion(combined)
-        }
-        
-        if let queue = queue {
-            group.notify(queue: queue, execute: submitResult)
+        if let queue {
+            waitAsync(on: group, with: values, queue: queue, timeout: timeout, completion: completion)
         } else {
-            group.wait()
-            submitResult()
+            completion(waitSync(on: group, with: values, timeout: timeout))
         }
+    }
+    
+    @inline(__always)
+    private func waitAsync(
+        on group: DispatchGroup,
+        with values: Values,
+        queue: DispatchQueue,
+        timeout: Timeout?,
+        completion: @escaping (Output) -> Void
+    ) {
+        let once = Closure(completion).oneShot()
+        
+        group.notify(queue: queue) { once(values.get(fallback: nil, combine: self.combine)) }
+        
+        if let timeout {
+            queue.asyncAfter(deadline: .now() + timeout.interval) {
+                once(values.get(fallback: timeout.fallback, combine: self.combine))
+            }
+        }
+    }
+    
+    @inline(__always)
+    private func waitSync(on group: DispatchGroup, with values: Values, timeout: Timeout?) -> Output {
+        let waitSucceeds = group.wait(interval: timeout?.interval) == .success
+        return values.get(fallback: waitSucceeds ? nil : timeout?.fallback, combine: combine)
     }
     
     // MARK: Subscribe
@@ -83,7 +106,7 @@ public class EventAskEx<Input, Transformed, Output> {
     public func subscribe(on queue: DispatchQueue = .global(), transform: @escaping AsyncTransform) -> SubscriptionToken {
         let subscription = DeinitAction {}
         let id = ObjectIdentifier(subscription)
-        _transforms.writeAsync {
+        transforms.writeAsync {
             $0[id] = (transform, queue)
         }
         subscription.replaceCleanup { [weak self] in self?.unsubscribe(id) }
@@ -95,14 +118,69 @@ public class EventAskEx<Input, Transformed, Output> {
     }
     
     private func unsubscribe(_ id: ObjectIdentifier) {
-        _transforms.writeAsync {
+        transforms.writeAsync {
             $0.removeValue(forKey: id)
         }
     }
+}
+
+extension EventAskEx {
+    public struct Timeout {
+        public var interval: TimeInterval
+        public var fallback: Fallback?
+        
+        public init(_ interval: TimeInterval, fallback: Fallback? = nil) {
+            self.interval = interval
+            self.fallback = fallback
+        }
+    }
     
-    // MARK: Private
-    
-    private typealias Entry<T> = (transform: AsyncTransform, queue: DispatchQueue)
-    private let _transforms = Synchronized<[ObjectIdentifier: Entry<AsyncTransform>]>(.concurrent)
-    private let _combine: ([Transformed]) -> Output
+    public enum Fallback {
+        case replaceMissed(Transformed)
+        case replaceOutput(Output)
+    }
+}
+
+extension EventAskEx {
+    private class Values {
+        private var values: [Transformed?]
+        private var lock = os_unfair_lock()
+        
+        init(count: Int) {
+            values = .init(repeating: nil, count: count)
+        }
+        
+        func set(value: Transformed, at index: Int) {
+            lock.withLock {
+                values[index] = value
+            }
+        }
+        
+        func get(fallback: Fallback?, combine: ([Transformed]) -> Output) -> Output {
+            switch fallback {
+            case .none:
+                let results = lock.withLock { values.compactMap { $0 } }
+                let combined = combine(results)
+                return combined
+            case .replaceMissed(let singleValue):
+                let results = lock.withLock { values.map { $0 ?? singleValue } }
+                let combined = combine(results)
+                return combined
+            case .replaceOutput(let wholeResult):
+                return wholeResult
+            }
+        }
+    }
+}
+
+extension DispatchGroup {
+    @inline(__always)
+    fileprivate func wait(interval: TimeInterval?) -> DispatchTimeoutResult {
+        if let interval {
+            return wait(timeout: .now() + interval)
+        } else {
+            wait()
+            return .success
+        }
+    }
 }
--- Tests/SwiftConvenienceTests/Observing/EventAskTests.swift
+++ Tests/SwiftConvenienceTests/Observing/EventAskTests.swift
@@ -3,38 +3,109 @@ import XCTest
 
 final class EventAskTests: XCTestCase {
     func test_emptyHandlers() {
-        let transformer = EventAsk<String, Int>()
+        let event = EventAsk<String, Int>()
         let exp = expectation(description: "Evaluated.")
-        transformer.askAsync("some event") {
+        
+        event.askAsync("some event") {
             XCTAssertTrue($0.isEmpty)
             exp.fulfill()
+        }
+        waitForExpectations()
+    }
+    
+    func test_oneToMany_multipleHandlers() {
+        var subscriptions: [SubscriptionToken] = []
+        let postedEvent = "some event"
+        let event = EventAsk<String, Int>()
+        event.subscribe { event in
+            XCTAssertEqual(event, postedEvent)
+            return 1
+        }
+        .store(in: &subscriptions)
+        event.subscribe { event, completion in
+            XCTAssertEqual(event, postedEvent)
+            completion(2)
+        }
+        .store(in: &subscriptions)
+        
+        let exp = expectation(description: "Evaluated.")
+        event.askAsync(postedEvent) {
+            XCTAssertEqual(Set($0), [1, 2])
+            exp.fulfill()
+        }
+        waitForExpectations()
+        
+        let syncResult = event.askSync(postedEvent)
+        XCTAssertEqual(Set(syncResult), [1, 2])
+    }
+    
+    func test_load() {
+        var subscriptions: [SubscriptionToken] = []
+        let postedEvent = "some event"
+        let event = EventAsk<String, Int>()
+        event.subscribe { event in
+            XCTAssertEqual(event, postedEvent)
+            return 1
+        }
+        .store(in: &subscriptions)
+        event.subscribe { event, completion in
+            XCTAssertEqual(event, postedEvent)
+            completion(2)
+        }
+        .store(in: &subscriptions)
+        
+        let count = 10_000
+        let exp = expectation(description: "Evaluated.")
+        exp.expectedFulfillmentCount = count
+        for _ in 0..<count {
+            DispatchQueue.global().async {
+                event.askAsync(postedEvent) {
+                    XCTAssertTrue($0.contains(1)) // response from first handler
+                    XCTAssertTrue($0.contains(2)) // response from second handler
+                    exp.fulfill()
+                }
+            }
         }
         
         waitForExpectations()
     }
     
-    func test_oneToMany_multipleHandlers() {
-        var disposables: [Any] = []
-        let postedEvent = "some event"
-        let transformer = EventAsk<String, Int>()
-        disposables.append(transformer.subscribe { event in
-            XCTAssertEqual(event, postedEvent)
+    func test_fallack() {
+        var subscriptions: [SubscriptionToken] = []
+        let event = EventAsk<String, Int>()
+        
+        // Set event processor that takes more time than timeout.
+        event.subscribe { _ in
+            Thread.sleep(forTimeInterval: 0.1)
             return 1
-        })
-        disposables.append(transformer.subscribe { event, completion in
-            XCTAssertEqual(event, postedEvent)
-            completion(2)
-        })
+        }.store(in: &subscriptions)
         
-        let exp = expectation(description: "Evaluated.")
-        transformer.askAsync(postedEvent) {
-            XCTAssertTrue($0.contains(1)) // response from first handler
-            XCTAssertTrue($0.contains(2)) // response from second handler
-            exp.fulfill()
-        }
+        // Set event processor that takes less time than timeout.
+        event.subscribe { _ in
+            Thread.sleep(forTimeInterval: 0.01)
+            return 2
+        }.store(in: &subscriptions)
         
-        withExtendedLifetime(disposables) {
-            _ = waitForExpectations()
+        // Assuming
+        let exp1 = expectation(description: "Evaluated.")
+        event.askAsync("", timeout: .init(0.05, fallback: nil)) {
+            XCTAssertEqual(Set($0), [2])
+            exp1.fulfill()
         }
+        waitForExpectations()
+        
+        let exp2 = expectation(description: "Evaluated.")
+        event.askAsync("", timeout: .init(0.05, fallback: .replaceMissed(10))) {
+            XCTAssertEqual(Set($0), [2, 10])
+            exp2.fulfill()
+        }
+        waitForExpectations()
+        
+        let exp3 = expectation(description: "Evaluated.")
+        event.askAsync("", timeout: .init(0.05, fallback: .replaceOutput([123]))) {
+            XCTAssertEqual(Set($0), [123])
+            exp3.fulfill()
+        }
+        waitForExpectations()
     }
 }
