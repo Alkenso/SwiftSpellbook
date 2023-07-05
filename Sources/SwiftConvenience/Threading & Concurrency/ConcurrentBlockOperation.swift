8a0bb21b2304f3be3bb5d27485fac53c7030da41
Alkenso (Vladimir Vashurkin) <alkensox@gmail.com>
2023-07-05 18:34:35 +0300

    Introduce `ConcurrentBlockOperation` subclass of (NS)Operation

---
+++ Sources/SwiftConvenience/Threading & Concurrency/ConcurrentBlockOperation.swift
@@ -0,0 +1,67 @@
+//  MIT License
+//
+//  Copyright (c) 2023 Alkenso (Vladimir Vashurkin)
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
+import Foundation
+
+public final class ConcurrentBlockOperation: Operation {
+    @Atomic private var state: Bool?
+    private let block: (ValueView<Bool>, @escaping () -> Void) -> Void
+    
+    public init(block: @escaping (_ isCancelled: ValueView<Bool>, _ completion: @escaping () -> Void) -> Void) {
+        self.block = block
+    }
+    
+    public override var isExecuting: Bool {
+        state == false
+    }
+    
+    public override var isFinished: Bool {
+        state == true
+    }
+    
+    public override var isAsynchronous: Bool { true }
+    
+    public override func start() {
+        guard !isCancelled else {
+            finish()
+            return
+        }
+        
+        willChangeValue(for: \.isExecuting)
+        state = false
+        didChangeValue(for: \.isExecuting)
+        
+        main()
+    }
+    
+    public override func main() {
+        block(.init { self.isCancelled }, finish)
+    }
+    
+    func finish() {
+        willChangeValue(for: \.isExecuting)
+        willChangeValue(for: \.isFinished)
+        state = true
+        didChangeValue(for: \.isExecuting)
+        didChangeValue(for: \.isFinished)
+    }
+}
---
+++ Tests/SwiftConvenienceTests/Threading & Concurrency/ConcurrentBlockOperationTests.swift
@@ -0,0 +1,47 @@
+import SwiftConvenience
+
+import Foundation
+import XCTest
+
+class ConcurrentBlockOperationTests: XCTestCase {
+    func test() throws {
+        let interval = 0.1
+        let op = ConcurrentBlockOperation { isCancelled, completion in
+            Thread.sleep(forTimeInterval: interval)
+            completion()
+        }
+        let queue = OperationQueue()
+        queue.addOperation(op)
+        
+        Thread.sleep(forTimeInterval: 0.05)
+        
+        XCTAssertTrue(op.isAsynchronous)
+        XCTAssertTrue(op.isReady)
+        XCTAssertTrue(op.isExecuting)
+        XCTAssertFalse(op.isFinished)
+        
+        Thread.sleep(forTimeInterval: interval)
+        
+        XCTAssertFalse(op.isExecuting)
+        XCTAssertTrue(op.isFinished)
+    }
+    
+    func test_cancel() throws {
+        let exp = expectation(description: "finished")
+        let op = ConcurrentBlockOperation { isCancelled, completion in
+            while !isCancelled.get() {
+                Thread.sleep(forTimeInterval: 0.01)
+            }
+            completion()
+            exp.fulfill()
+        }
+        let queue = OperationQueue()
+        queue.addOperation(op)
+        
+        DispatchQueue.global().asyncAfter(interval: 0.1) {
+            op.cancel()
+        }
+        
+        waitForExpectations(timeout: 0.2)
+    }
+}
