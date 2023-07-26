79091d01cebca5dfe35f2f4f9ec76f2c3bf974c7
Alkenso (Vladimir Vashurkin) <alkensox@gmail.com>
2023-07-26 11:39:58 +0300

    Add Task extensions

---
+++ Sources/SwiftConvenience/Threading & Concurrency/Extensions - Task.swift
@@ -0,0 +1,87 @@
+//  MIT License
+//
+//  Copyright (c) 2022 Alkenso (Vladimir Vashurkin)
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
+import Foundation
+
+@available(macOS 10.15, iOS 13, tvOS 13.0, watchOS 6.0, *)
+extension Task {
+    public static func runWithCompletion<R>(
+        _: R.Type = R.Type,
+        _ body: @escaping () async throws -> R,
+        completion: @escaping (Result<R, Error>) -> Void
+    ) where Success == Void, Failure == Never {
+        Task<Void, Never> {
+            do {
+                let result = try await body()
+                completion(.success(result))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    public static func runWithCompletion<R>(
+        _ body: @escaping () async -> R,
+        completion: @escaping (R) -> Void
+    ) where Success == Void, Failure == Never {
+        Task<Void, Never> {
+            completion(await body())
+        }
+    }
+    
+    public static func runWithCompletion(
+        _ body: @escaping () async throws -> Void,
+        completion: @escaping (Error?) -> Void
+    ) where Success == Void, Failure == Never {
+        Task<Void, Never> {
+            do {
+                try await body()
+                completion(nil)
+            } catch {
+                completion(error)
+            }
+        }
+    }
+}
+
+@available(macOS 10.15, iOS 13, tvOS 13.0, watchOS 6.0, *)
+extension Task where Success == Never, Failure == Never {
+    public static func sleep(forTimeInterval interval: TimeInterval) async throws {
+        try await sleep(nanoseconds: UInt64(interval * TimeInterval(NSEC_PER_SEC)))
+    }
+    
+    @available(macOS, deprecated: 13.0, message: "Use `sleep(for:)` method instead")
+    @available(iOS, deprecated: 16.0, message: "Use `sleep(for:)` method instead")
+    @available(watchOS, deprecated: 9.0, message: "Use `sleep(for:)` method instead")
+    @available(tvOS, deprecated: 16.0, message: "Use `sleep(for:)` method instead")
+    public static func sleep(seconds duration: Int) async throws {
+        try await sleep(nanoseconds: UInt64(duration) * NSEC_PER_SEC)
+    }
+    
+    @available(macOS, deprecated: 13.0, message: "Use `sleep(for:)` method instead")
+    @available(iOS, deprecated: 16.0, message: "Use `sleep(for:)` method instead")
+    @available(watchOS, deprecated: 9.0, message: "Use `sleep(for:)` method instead")
+    @available(tvOS, deprecated: 16.0, message: "Use `sleep(for:)` method instead")
+    public static func sleep(milliseconds duration: Int) async throws {
+        try await sleep(nanoseconds: UInt64(duration) * NSEC_PER_MSEC)
+    }
+}
