499fab72588cc429c01c63f98a145156fdeb8baa
Alkenso (Vladimir Vashurkin) <alkensox@gmail.com>
2023-10-17 18:08:37 +0300

    Implement beta version of HTTPClient

---
+++ Sources/SwiftConvenience/Network/HTTPClient.swift
@@ -0,0 +1,115 @@
+//  MIT License
+//
+//  Copyright (c) 2023 Alkenso (Vladimir Vashurkin)
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
+import Foundation
+
+open class HTTPClient {
+    private let session: URLSession
+    
+    public init(session: URLSession = .shared) {
+        self.session = session
+    }
+}
+ 
+extension HTTPClient {
+    public func data(for request: HTTPRequest, completion: @escaping (Result<HTTPResult<Data>, Error>) -> Void) {
+        let urlRequest: URLRequest
+        do {
+            urlRequest = try request.urlRequest()
+        } catch {
+            completion(.failure(error))
+            return
+        }
+        
+        session.dataTask(with: urlRequest) { data, response, error in
+            if let error {
+                completion(.failure(error))
+                return
+            }
+            guard let response = response as? HTTPURLResponse else {
+                completion(.failure(URLError.badResponseType(response)))
+                return
+            }
+            completion(.success(.init(value: data ?? Data(), response: response)))
+        }.resume()
+    }
+    
+    public func object<T>(
+        for request: HTTPRequest,
+        decoder: ObjectDecoder<T>,
+        completion: @escaping (Result<HTTPResult<T>, Error>) -> Void
+    ) {
+        data(for: request) {
+            completion($0.flatMap { dataResult in
+                Self.decodeResponse(dataResult.value, decoder: decoder)
+                    .map { .init(value: $0, response: dataResult.response) }
+            })
+        }
+    }
+    
+    private static func decodeResponse<T>(_ data: Data, decoder: ObjectDecoder<T>) -> Result<T, Error> {
+        do {
+            let object = try decoder.decode(T.self, data)
+            return .success(object)
+        } catch {
+            return .failure(URLError.badResponse(error))
+        }
+    }
+}
+
+@available(macOS 12.0, iOS 15, tvOS 15.0, watchOS 8.0, *)
+extension HTTPClient {
+    public func data(
+        for request: HTTPRequest,
+        delegate: URLSessionTaskDelegate? = nil
+    ) async throws -> HTTPResult<Data> {
+        let urlRequest = try request.urlRequest()
+        let (data, response) = try await session.data(for: urlRequest, delegate: delegate)
+        guard let response = response as? HTTPURLResponse else {
+            throw URLError.badResponseType(response)
+        }
+        
+        return .init(value: data, response: response)
+    }
+    
+    public func object<T>(
+        for request: HTTPRequest,
+        delegate: URLSessionTaskDelegate? = nil,
+        decoder: ObjectDecoder<T>
+    ) async throws -> HTTPResult<T> {
+        let dataResult = try await data(for: request, delegate: delegate)
+        let object = try Self.decodeResponse(dataResult.value, decoder: decoder).get()
+        return .init(value: object, response: dataResult.response)
+    }
+}
+
+extension URLError {
+    fileprivate static func badResponse(_ underlyingError: Error) -> URLError {
+        URLError(.badServerResponse, userInfo: [NSUnderlyingErrorKey: underlyingError])
+    }
+    
+    fileprivate static func badResponseType(_ response: URLResponse?) -> URLError {
+        URLError(.badServerResponse, userInfo: [
+            NSUnderlyingErrorKey: CommonError.cast(name: "HTTPResponse", response, to: HTTPURLResponse.self)
+        ])
+    }
+}
---
+++ Sources/SwiftConvenience/Network/HTTPRequest.swift
@@ -0,0 +1,185 @@
+//  MIT License
+//
+//  Copyright (c) 2023 Alkenso (Vladimir Vashurkin)
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
+import Foundation
+
+public struct HTTPRequest {
+    public var url: String
+    public var method: Method
+    public var port: UInt16?
+    
+    public var query: [QueryItem: String] = [:]
+    public var headers: [Header: HeaderValue] = [:]
+    public var body: Body?
+    
+    public init(urlString: String, method: Method) {
+        self.url = urlString
+        self.method = method
+    }
+    
+    public init(url: URL, method: Method) {
+        self.init(urlString: url.absoluteString, method: method)
+    }
+}
+
+extension HTTPRequest {
+    public struct Method: RawRepresentable, Hashable {
+        public var rawValue: String
+        public init(rawValue: String) { self.rawValue = rawValue }
+    }
+}
+
+extension HTTPRequest.Method {
+    public static let get = Self(rawValue: "GET")
+    public static let post = Self(rawValue: "POST")
+}
+    
+extension HTTPRequest {
+    public struct QueryItem: RawRepresentable, Hashable, ExpressibleByStringLiteral {
+        public var rawValue: String
+        public init(rawValue: String) { self.rawValue = rawValue }
+        public init(stringLiteral value: StringLiteralType) { self.rawValue = value }
+    }
+}
+ 
+extension HTTPRequest {
+    public struct Header: RawRepresentable, Hashable {
+        public var rawValue: String
+        public init(rawValue: String) { self.rawValue = rawValue }
+    }
+    
+    public struct HeaderValue: RawRepresentable, Hashable {
+        public var rawValue: String
+        public init(rawValue: String) { self.rawValue = rawValue }
+    }
+}
+
+extension HTTPRequest.Header {
+    public static let userAgent = Self(rawValue: "user-agent")
+}
+
+extension HTTPRequest.Header: ExpressibleByStringLiteral {
+    public init(stringLiteral value: StringLiteralType) { self.rawValue = value }
+}
+
+extension HTTPRequest.HeaderValue {
+    public static func authorization(_ type: HTTPRequest.AuthorizationType, _ token: String) -> Self {
+        .init(rawValue: "\(type.rawValue) \(token)")
+    }
+}
+
+extension HTTPRequest.HeaderValue: ExpressibleByStringLiteral {
+    public init(stringLiteral value: StringLiteralType) { self.rawValue = value }
+}
+
+extension HTTPRequest.HeaderValue: ExpressibleByStringInterpolation {
+    public init(stringInterpolation: DefaultStringInterpolation) { self.rawValue = stringInterpolation.description }
+}
+
+extension HTTPRequest {
+    public struct AuthorizationType: RawRepresentable, Hashable {
+        public var rawValue: String
+        public init(rawValue: String) { self.rawValue = rawValue }
+    }
+}
+
+extension HTTPRequest.AuthorizationType: ExpressibleByStringLiteral {
+    public init(stringLiteral value: StringLiteralType) { self.rawValue = value }
+}
+
+extension HTTPRequest.AuthorizationType: ExpressibleByStringInterpolation {
+    public init(stringInterpolation: DefaultStringInterpolation) { self.rawValue = stringInterpolation.description }
+}
+
+extension HTTPRequest {
+    public struct Body {
+        internal let data: () throws -> Data
+        internal let contentType: String?
+        
+        public init(contentType: String?, data: @escaping () throws -> Data) {
+            self.data = data
+            self.contentType = contentType
+        }
+    }
+}
+
+extension HTTPRequest.Body {
+    private static let contentTypePlist = "application/x-apple-plist"
+    private static let contentTypeJSON = "application/json"
+    
+    private init<T>(value: T, encoder: ObjectEncoder<T>, contentType: String) {
+        self.init(contentType: contentType) { try encoder.encode(value) }
+    }
+    
+    public static func foundation(json obj: Any, options: JSONSerialization.WritingOptions = []) -> Self {
+        .init(value: obj, encoder: .foundationJSON(options: options), contentType: contentTypeJSON)
+    }
+    
+    public static func foundation(plist obj: Any, format: PropertyListSerialization.PropertyListFormat = .xml) -> Self {
+        .init(value: obj, encoder: .foundationPlist(format: format), contentType: contentTypePlist)
+    }
+    
+    public static func codable<T: Encodable>(json obj: T, formatting: JSONEncoder.OutputFormatting = []) -> Self {
+        .init(value: obj, encoder: .json(formatting: formatting), contentType: contentTypeJSON)
+    }
+    
+    public static func codable<T: Encodable>(plist obj: T, format: PropertyListSerialization.PropertyListFormat = .xml) -> Self {
+        .init(value: obj, encoder: .plist(format: format), contentType: contentTypePlist)
+    }
+    
+    public static func data(_ data: Data, contentType: String?) -> Self {
+        .init(contentType: contentType, data: { data })
+    }
+}
+
+extension HTTPRequest {
+    public func urlRequest() throws -> URLRequest {
+        guard var components = URLComponents(string: url) else {
+            fatalError()
+        }
+        if !query.isEmpty {
+            components.queryItems = query.map { URLQueryItem(name: $0.key.rawValue, value: $0.value) }
+        }
+        if let port  {
+            components.port = Int(port)
+        }
+        guard let url = components.url else {
+            fatalError()
+        }
+        
+        var urlRequest = URLRequest(url: url)
+        headers.forEach {
+            urlRequest.setValue($0.value.rawValue, forHTTPHeaderField: $0.key.rawValue)
+        }
+        urlRequest.httpMethod = method.rawValue
+        if let body {
+            urlRequest.httpBody = try Result { try body.data() }
+                .mapError { $0 }
+                .get()
+            if let contentType = body.contentType {
+                urlRequest.addValue(contentType, forHTTPHeaderField: "Content-Type")
+            }
+        }
+        
+        return urlRequest
+    }
+}
