9a4146535182c660f0d91248cad98d8f7fceb9e6
Alkenso (Vladimir Vashurkin) <alkensox@gmail.com>
2024-12-19 08:58:08 +0200

    Introduce FileManager.uniqueFile to generate file with unique name

--- Tests/SpellbookTests/Filesystem & Bundle/FileManagerTests.swift
+++ Tests/SpellbookTests/Filesystem & Bundle/FileManagerTests.swift
@@ -59,4 +59,31 @@ class FileManagerExtensionsTests: XCTestCase {
         try FileManager.default.removeXattr(at: file, name: "xa1")
         XCTAssertEqual(try FileManager.default.listXattr(at: file), ["xa2"])
     }
+    
+    func test_uniqueFile() {
+        let fm = FileManager.default
+        func createFile(_ name: String) -> Bool {
+            fm.createFile(atPath: tempDir.location.appendingPathComponent(name).path, contents: .random(20))
+        }
+        
+        XCTAssertEqual(fm.uniqueFile("test.foo", in: tempDir.location).lastPathComponent, "test.foo")
+        // Ensure it is not created.
+        XCTAssertEqual(fm.uniqueFile("test.foo", in: tempDir.location).lastPathComponent, "test.foo")
+        
+        XCTAssertTrue(createFile("test.foo"))
+        XCTAssertEqual(fm.uniqueFile("test.foo", in: tempDir.location).lastPathComponent, "test_1.foo")
+        
+        XCTAssertTrue(createFile("test.foo.bar"))
+        XCTAssertEqual(fm.uniqueFile("test.foo.bar", in: tempDir.location).lastPathComponent, "test.foo_1.bar")
+        
+        XCTAssertEqual(fm.uniqueFile("test", in: tempDir.location).lastPathComponent, "test")
+        XCTAssertTrue(createFile("test"))
+        XCTAssertTrue(createFile("test_1"))
+        XCTAssertTrue(createFile("test_3"))
+        XCTAssertEqual(fm.uniqueFile("test", in: tempDir.location).lastPathComponent, "test_2")
+        
+        XCTAssertEqual(fm.uniqueFile("dir/", in: tempDir.location).lastPathComponent, "dir")
+        XCTAssertTrue(createFile("dir"))
+        XCTAssertEqual(fm.uniqueFile("dir/", in: tempDir.location).lastPathComponent, "dir_1")
+    }
 }
