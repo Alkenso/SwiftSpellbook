a6e6ba8caf8b5ab83dadacccef36cc280b185482
Alkenso (Vladimir Vashurkin) <alkensox@gmail.com>
2025-02-14 10:58:30 +0200

    Extend RGBColor struct

--- Sources/SpellbookFoundation/GUI/RGBColor.swift
+++ Sources/SpellbookFoundation/GUI/RGBColor.swift
@@ -15,7 +15,12 @@ public struct RGBColor: Hashable, Codable {
     public var blue: CGFloat
     public var alpha: CGFloat
     
-    public init(red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat = 1.0) {
+    public init(
+        @Clamped(0...1) red: CGFloat,
+        @Clamped(0...1) green: CGFloat,
+        @Clamped(0...1) blue: CGFloat,
+        @Clamped(0...1) alpha: CGFloat = 1.0
+    ) {
         self.red = red
         self.green = green
         self.blue = blue
@@ -24,10 +29,88 @@ public struct RGBColor: Hashable, Codable {
 }
 
 extension RGBColor {
+    public init?(hex hexColor: String, alphaFirst: Bool = false) {
+        var hexColor = hexColor
+        if hexColor.hasPrefix("#") {
+            hexColor.removeFirst()
+        }
+        
+        let hasAlpha = hexColor.count == 8
+        guard hexColor.count == 6 || hasAlpha else { return nil }
+        
+        func popColorComponent() -> CGFloat? {
+            guard hexColor.count >= 2 else { return nil }
+            
+            let to = hexColor.index(hexColor.startIndex, offsetBy: 1)
+            let str = hexColor[hexColor.startIndex...to]
+            hexColor.removeFirst(2)
+            
+            let int = Int(str, radix: 16)
+            return int.flatMap { CGFloat($0) / 255 }
+        }
+        
+        var a: CGFloat?
+        if hasAlpha, alphaFirst {
+            a = popColorComponent()
+        }
+        guard let r = popColorComponent(),
+              let g = popColorComponent(),
+              let b = popColorComponent()
+        else {
+            return nil
+        }
+        if a == nil {
+            a = popColorComponent()
+        }
+        
+        self.init(red: r, green: g, blue: b, alpha: a ?? 1.0)
+    }
+}
+
+extension RGBColor {
+    public init?(cgColor: CGColor) {
+        guard let rgb = cgColor.converted(
+            to: CGColorSpaceCreateDeviceRGB(),
+            intent: .defaultIntent,
+            options: nil
+        ) else { return nil }
+        guard let components = rgb.components, components.count >= 3 else { return nil }
+        self.init(red: components[0], green: components[1], blue: components[2], alpha: rgb.alpha)
+    }
+    
     public var cgColor: CGColor {
         CGColor(red: red, green: green, blue: blue, alpha: alpha)
     }
 }
+
+#if canImport(AppKit)
+
+import AppKit
+
+extension RGBColor {
+    public init(_ nsColor: NSColor) {
+        self.init(
+            red: nsColor.redComponent,
+            green: nsColor.greenComponent,
+            blue: nsColor.blueComponent,
+            alpha: nsColor.alphaComponent
+        )
+    }
+}
+
+#endif
+
+#if canImport(UIKit)
+
+import UIKit
+
+extension RGBColor {
+    public init?(_ uiColor: UIColor) {
+        self.init(cgColor: uiColor.cgColor)
+    }
+}
+
+#endif
 
 extension RGBColor {
     public static func random(
--- Tests/SpellbookTests/GUITests/GUITests.swift
+++ Tests/SpellbookTests/GUITests/GUITests.swift
@@ -30,4 +30,20 @@ class GUITests: XCTestCase {
         
         XCTAssertEqual(rect.flippedY(fullHeight: 400).flippedY(fullHeight: 400), rect)
     }
+    
+    func test_RGBColor() {
+        XCTAssertEqual(RGBColor(hex: ""), nil)
+        XCTAssertEqual(RGBColor(hex: "#"), nil)
+        XCTAssertEqual(RGBColor(hex: "#FFF"), nil)
+        XCTAssertEqual(RGBColor(hex: "#AABBCCDDE"), nil)
+        XCTAssertEqual(RGBColor(hex: "#AABBCCE"), nil)
+        
+        XCTAssertEqual(RGBColor(hex: "#66FFCC"), RGBColor(red: 0x66 / 255.0, green: 1.0, blue: 0xCC / 255.0))
+        XCTAssertEqual(RGBColor(hex: "66FFCC"), RGBColor(red: 0x66 / 255.0, green: 1.0, blue: 0xCC / 255.0))
+        XCTAssertEqual(RGBColor(hex: "#66FFCC", alphaFirst: true), RGBColor(red: 0x66 / 255.0, green: 1.0, blue: 0xCC / 255.0))
+        
+        XCTAssertEqual(RGBColor(hex: "#66FFCC80"), RGBColor(red: 0x66 / 255.0, green: 1.0, blue: 0xCC / 255.0, alpha: 0x80 / 255.0))
+        XCTAssertEqual(RGBColor(hex: "66FFCC80"), RGBColor(red: 0x66 / 255.0, green: 1.0, blue: 0xCC / 255.0, alpha: 0x80 / 255.0))
+        XCTAssertEqual(RGBColor(hex: "#8066FFCC", alphaFirst: true), RGBColor(red: 0x66 / 255.0, green: 1.0, blue: 0xCC / 255.0, alpha: 0x80 / 255.0))
+    }
 }
